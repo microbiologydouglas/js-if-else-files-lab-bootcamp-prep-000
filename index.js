0a9cfe904a10339024732ef2ecc7e9c2cd9bea4d
Douglas Whalen <microbiologydouglas@yahoo.com>
2017-12-12 20:04:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (name === "Susan") {
 }
 if (height === "70") {
   console.log("The height is correct.")
-} else { console.log("Expected the height to be "70.")
+} else { console.log("Expected the height to be 70.")
 }
 // Don't worry about this
 module.exports = { name, height
